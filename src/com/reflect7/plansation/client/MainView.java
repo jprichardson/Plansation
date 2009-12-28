1f306d1fdfb56298e52d7638da513e7e9eda31dd
JP <jp@reflect7.com>
2009-12-28 17:43:41 -0600

    Fixed Splitter Background Color. Hack

--- src/com/reflect7/plansation/client/MainView.java
+++ src/com/reflect7/plansation/client/MainView.java
@@ -1,13 +1,19 @@
 package com.reflect7.plansation.client;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.DivElement;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.Node;
+import com.google.gwt.dom.client.NodeList;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.SplitLayoutPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class MainView extends Composite {
@@ -17,9 +23,31 @@ public class MainView extends Composite {
 
 	interface MainViewUiBinder extends UiBinder<Widget, MainView> {}
 
+	@UiField SplitLayoutPanel splitterPanel;
 
 	public MainView() {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
 
+	@Override
+	public void onAttach(){
+		super.onAttach();
+		
+		//hack until GWT Team fixes Issue 4384/4417
+		Element splitterElement = splitterPanel.getElement();
+		NodeList<Node> nodes = splitterElement.getChildNodes();
+		//Window.alert(nodes.getLength() + "");
+		for (int x = 0; x < nodes.getLength(); ++x)
+			if (nodes.getItem(x) instanceof Element){
+				Element e = (Element)nodes.getItem(x);
+				String s = e.getFirstChildElement().getClassName();
+				//Window.alert(s);
+				if (s != null)
+					if (s.equals("gwt-SplitLayoutPanel-HDragger") || s.equals("gwt-SplitLayoutPanel-VDragger")){
+						e.getFirstChildElement().getStyle().clearBackgroundColor();
+					}
+						
+			}
+				
+	}
 }
