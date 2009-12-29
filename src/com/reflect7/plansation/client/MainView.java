48208de04b35245d2b506207bc172a34952faa0d
JP <jp@reflect7.com>
2009-12-29 13:59:08 -0600

    Added tasks to task tree

--- src/com/reflect7/plansation/client/MainView.java
+++ src/com/reflect7/plansation/client/MainView.java
@@ -5,7 +5,9 @@ import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.Node;
 import com.google.gwt.dom.client.NodeList;
+import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.KeyPressEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
@@ -14,6 +16,9 @@ import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.SplitLayoutPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Tree;
+import com.google.gwt.user.client.ui.TreeItem;
 import com.google.gwt.user.client.ui.Widget;
 
 public class MainView extends Composite {
@@ -24,7 +29,10 @@ public class MainView extends Composite {
 	interface MainViewUiBinder extends UiBinder<Widget, MainView> {}
 
 	@UiField SplitLayoutPanel splitterPanel;
-
+	@UiField Tree treeTasks;
+	@UiField TextBox textTask;
+	
+	
 	public MainView() {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
@@ -47,7 +55,30 @@ public class MainView extends Composite {
 						e.getFirstChildElement().getStyle().clearBackgroundColor();
 					}
 						
-			}
-				
+			}	
 	}
+	
+	@UiHandler("textTask")
+	void handleChange(ChangeEvent e){
+		
+	}
+	
+	@UiHandler("textTask")
+	void handleKeyPress(KeyPressEvent e){
+		byte b = (byte)e.getCharCode();
+		if (b == 13){//[ENTER] key pressed
+			String task = textTask.getText().trim();
+			TreeItem newItem = new TreeItem(task);
+			
+			TreeItem selectedItem = treeTasks.getSelectedItem();
+			if (selectedItem != null)
+				selectedItem.addItem(newItem);
+			else
+				treeTasks.addItem(newItem);
+			
+			textTask.setText("");
+		}
+	}
+	
+	
 }
