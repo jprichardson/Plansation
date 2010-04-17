9958c49273bec97f1e88083ee2bd3b1d761b79b6
JP <jp@reflect7.com>
2010-04-17 02:16:29 -0500

    before removal of JDO

--- src/com/reflect7/plansation/server/GreetingServiceImpl.java
+++ src/com/reflect7/plansation/server/GreetingServiceImpl.java
@@ -10,7 +10,9 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 public class GreetingServiceImpl extends RemoteServiceServlet implements
 		GreetingService {
 
-	public String greetServer(String input) {
+	public String greetServer(String input) throws IllegalArgumentException {
+
+
 		String serverInfo = getServletContext().getServerInfo();
 		String userAgent = getThreadLocalRequest().getHeader("User-Agent");
 		return "Hello, " + input + "!<br><br>I am running " + serverInfo
